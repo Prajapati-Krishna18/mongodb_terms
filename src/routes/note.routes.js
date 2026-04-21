b929dcd02cc26ee682bc294f3bdf013161efdc7e
Krishna Prajapati <krishna.prajapati.rcg@gmail.com>
2026-04-21 23:30:20 +0530

    feat: add bulk create notes endpoint (POST /api/notes/bulk)

--- src/routes/note.routes.js
+++ src/routes/note.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { createNote } = require("../controllers/note.controller");
+const { createNote, createBulkNotes } = require("../controllers/note.controller");
 
 // CRUD
+router.post("/bulk", createBulkNotes);
 router.post("/", createNote);
 
 module.exports = router;
